0324bae0dd28857a7481c2963f9826206ecc615d
developerharo <developerharo@gmail.com>
2021-09-14 23:31:40 -0500

    Seccion 5. Creando Proyectos. 14. Metodo Destroy

--- rest-api/start/routes.js
+++ rest-api/start/routes.js
@@ -21,4 +21,7 @@ Route.group(() => {
   Route.post("users/login", "UserController.login");
   Route.get("proyectos", "ProyectoController.index").middleware("auth");
   Route.post("proyectos", "ProyectoController.create").middleware("auth");
+  Route.delete("proyectos/:id", "ProyectoController.destroy").middleware(
+    "auth"
+  );
 }).prefix("api/v1/");
